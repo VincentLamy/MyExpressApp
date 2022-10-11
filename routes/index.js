526cf207ad0c62a437c2a83244871a85ab556030
Vincent <202045777@cegepsherbrooke.qc.ca>
2022-10-11 11:06:04 -0400

    Modification index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Ma super application Express', header: 'Lab 04' });
 });
 
 module.exports = router;
